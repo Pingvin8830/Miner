50916da1949814abad65c66a2ed79ad28db27998
Vlasov Anton <Vlasov-Ant@yandex.ru>
2017-05-30 22:49:00 +0300

    Красявости логики

--- classes.py
+++ classes.py
@@ -5,36 +5,62 @@ from random import randrange
 class Field (object):
   '''Игровое поле'''
   def __init__ (self, rows = 10, columns = 10, mines = 10):
-    self.rows       = rows
-    self.columns    = columns
-    self.mines      = mines
-    self.list_cells = {}
+    '''Инициализация поля'''
+    self.rows       = rows           # Количество строк
+    self.columns    = columns        # Количество столбцов
+    self.mines      = mines          # Количество мин
+    self.closes     = rows * columns # Количество закрытых ячеек
+    self.list_cells = {}             # Ячейки
 
+    # Составление списка ячеек
     for y in range (rows):
       for x in range (columns):
-        self.list_cells [(x, y)] = Cell (x, y)
+        self.list_cells [(x, y)] = Cell ()
+
+  def clear (self):
+    '''Очистка поля'''
+
+    # Сброс количества закрытых ячеек
+    self.closes = self.rows * self.columns
+
+    # Сброс самих ячеек
+    for y in range (self.rows):
+      for x in range (self.columns):
+        self.list_cells [(x, y)].__init__ ()
+
 
   def set_mines (self):
-    count = 0
+    '''Расстановка мин'''
+    count = 0 # Количество расставленных мин
+
+    # "Перебор мин"
     while count < self.mines:
-      cell = self.list_cells [(randrange (self.columns), randrange (self.rows))]
+
+      # Выбор случайной ячейки
+      mines_x = randrange (self.columns)
+      mines_y = randrange (self.rows)
+      cell    = self.list_cells [(mines_x, mines_y)]
+
+      # Контроль "незанятости"
       if not cell.is_mine:
         cell.is_mine = True
+
+        # Обновление чисел вокруг мины
         for y in range (3):
           for x in range (3):
             try:
-              cell_update = self.list_cells [(cell.x - 1 + x, cell.y - 1 + y)]
+              cell_update = self.list_cells [(mines_x - 1 + x, mines_y - 1 + y)]
               if not cell_update.is_mine: cell_update.count += 1
             except:
               continue
-        cell.count   = 9
-        count       += 1
+        cell.count = 9
+        count     += 1
 
   def __str__ (self):
     string = '  '
-    delim = ' +'
+    delim  = ' +'
     for x in range (self.columns):
-      delim += '---+'
+      delim  += '---+'
       string += ' %d  ' % x
     string += '\n' + delim + '\n'
     for y in range (self.rows):
@@ -48,15 +74,15 @@ class Field (object):
     return string
 
 class Cell (object):
-  def __init__ (self, x, y):
-    self.x       = x
-    self.y       = y
-    self.count   = 0
-    self.is_mine = False
-    self.is_open = False
+  '''Ячейка поля'''
+  def __init__ (self):
+    '''Инициализация ячейки'''
+    self.count   = 0      # Число в ячейке
+    self.is_mine = False  # Признак мины
+    self.is_open = False  # Признак "открытости"
 
   def __str__ (self):
-    return '(%d, %d)' % (self.x, self.y)
+    return '%d' % self.count
 
 if __name__ == '__main__':
   print ('Это модуль с классами')
