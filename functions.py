3e84121630165289e10125cee84708f820e879c1
Vlasov Anton <Vlasov-Ant@yandex.ru>
2017-06-03 00:38:13 +0300

    Пока не знаю зачем

--- functions.py
+++ functions.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
-def control_count (count, control, integer = True):
+def control_count (count, control, minimum = 0, maximum = 10, integer = True):
   '''Функция контроля правильности числовых данных'''
 
   if integer: count = int   (count)
   else:       count = float (count)
 
   # Контроль на положительное число
-  if control == 'positive': return count > 0
-  else:                     return False
+  if   control == 'positive': return count > 0
+  elif control == 'in':       return count in range ()
+  else:                       return False
 
 if __name__ == '__main__':
   print ('Это модуль с функциями')
